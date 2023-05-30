56f9cdac844e5b45cab3f5d0394e5f2c4ba0739c
Zeifyra <s.rodriguez63@uniandes.edu.co>
2023-05-29 20:49:12 -0500

    Especialidad

--- pymongo_example/pymongoexample/main.py
+++ pymongo_example/pymongoexample/main.py
@@ -7,6 +7,6 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     user_collection = mongo.db.users
-    user_collection.insert({'name' : 'Cristina'})
-    user_collection.insert({'name' : 'Derek'})
-    return '<h1>Added a User!</h1>'
+    user_collection.insert({'name' : 'Cristina','especialidad':'Medicina General'})
+    user_collection.insert({'name' : 'Derek','especialidad':'Oncologia'})
+    return '<h1>Added a Medic!</h1>'
